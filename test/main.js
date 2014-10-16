7579dda6c2328e6dbf55238d78d9572ed93fb7eb
Greduan <eduanlavaque@gmail.com>
2014-10-15 20:53:53 -0500

    Covered an edge case, updated deps and more tests

--- test/main.js
+++ test/main.js
@@ -4,22 +4,22 @@ var main = require('..');
 test('main.getLastNumber: make sure it gets the correct number', function (t) {
     'use strict';
 
-    t.plan(2);
+    t.plan(4);
 
-    var exampleFile1, result1;
+    var exampleFile1, exampleFile2, result1, result2;
 
     exampleFile1  = '1.\n2.\n3.\n6.\n';
-    // var exampleFile2 = '1.\n2.\n3.\n6.';
+    exampleFile2 = '1.\n2.\n3.\n6.';
 
     result1 = main.getLastNumber(exampleFile1);
-    // var result2 = main.getLastNumber(exampleFile2);
+    result2 = main.getLastNumber(exampleFile2);
 
     // make sure they're numbers and they return the right numbers
     t.equal(typeof result1, 'number');
     t.equal(result1, 6);
 
-    // t.equal(typeof result2, 'number');
-    // t.equal(result2, 6);
+    t.equal(typeof result2, 'number');
+    t.equal(result2, 6);
 
     t.end();
 });
